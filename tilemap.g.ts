e54f88c33f225f656b62de67446b9a0ac11cfc5b
RunningdustSC <118071886+RunningdustSC@users.noreply.github.com>
2022-11-12 12:45:37 -0500

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -4,13 +4,29 @@ namespace myTiles {
     export const tile1 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "myTile":
+            case "NE Road":
             case "tile1":return tile1;
-            case "myTile0":
+            case "SW Road":
             case "tile2":return tile2;
+            case "SE Road":
+            case "tile4":return tile4;
+            case "NW Road":
+            case "tile3":return tile3;
+            case "Vertical Road":
+            case "tile5":return tile5;
+            case "Horizontal Road":
+            case "tile6":return tile6;
         }
         return null;
     })
